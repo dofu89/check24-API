7debb555c0abf678d7b4924d773edf81cb77f613
Fuad Osmanovic <57088641+dofu89@users.noreply.github.com>
2022-10-06 20:51:50 +0200

    more schemas and routes (#2)
    * more schemas and routes
    
    * err middleware

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,30 +1,127 @@
-import Product from '../models/productModel.js'
+import {
+  BeautyProduct,
+  TechnoProduct,
+  HouseholdProduct,
+} from '../models/productModel.js'
 
-export const createProduct = async (req, res, next) => {
-  const newProduct = new Product(req.body)
+//////POST//////
 
+export const createTechnoProduct = async (req, res, next) => {
+  const newProduct = new TechnoProduct(req.body)
   try {
     const savedProduct = await newProduct.save()
     res.status(200).json(savedProduct)
   } catch (err) {
-    res.send(err)
+    next(err)
   }
 }
 
-export const getProduct = async (req, res, next) => {
+export const createBeautyProduct = async (req, res, next) => {
+  const newProduct = new BeautyProduct(req.body)
   try {
-    const product = await Product.findById(req.params.id)
-    res.status(200).json(product)
+    const savedProduct = await newProduct.save()
+    res.status(200).json(savedProduct)
   } catch (err) {
-    console.log(err)
+    next(err)
   }
 }
 
-export const getAllProducts = async (req, res, next) => {
+export const createHouseholdProduct = async (req, res, next) => {
+  const newProduct = new HouseholdProduct(req.body)
   try {
-    const products = await Product.find()
+    const savedProduct = await newProduct.save()
+    res.status(200).json(savedProduct)
+  } catch (err) {
+    next(err)
+  }
+}
+
+//////GET//////
+
+//all techno
+export const getTechnoProducts = async (req, res, next) => {
+  const products = await TechnoProduct.find()
+  try {
     res.status(200).json(products)
   } catch (err) {
-    console.log(err)
+    next(err)
+  }
+}
+
+//single techno
+export const getTechnoProduct = async (req, res, next) => {
+  try {
+    const product = await TechnoProduct.findById(req.params.id)
+    res.status(200).json(product)
+  } catch (err) {
+    next(err)
+  }
+}
+
+//all beauty
+export const getBeautyProducts = async (req, res, next) => {
+  const products = await BeautyProduct.find()
+  try {
+    res.status(200).json(products)
+  } catch (err) {
+    next(err)
+  }
+}
+
+//single beauty
+export const getBeautyProduct = async (req, res, next) => {
+  try {
+    const product = await BeautyProduct.findById(req.params.id)
+    res.status(200).json(product)
+  } catch (err) {
+    next(err)
+  }
+}
+//all beauty
+export const getHouseholdProducts = async (req, res, next) => {
+  const products = await HouseholdProduct.find()
+  try {
+    res.status(200).json(products)
+  } catch (err) {
+    next(err)
+  }
+}
+
+//single beauty
+export const getHouseholdProduct = async (req, res, next) => {
+  try {
+    const product = await HouseholdProduct.findById(req.params.id)
+    res.status(200).json(product)
+  } catch (err) {
+    next(err)
+  }
+}
+
+//get all products
+export const getAllProducts = async (req, res, next) => {
+  const techno = await TechnoProduct.find()
+  const beauty = await BeautyProduct.find()
+  const haushold = await HouseholdProduct.find()
+  const productsDB = techno.concat(beauty).concat(haushold)
+  try {
+    const allProducts = productsDB
+    res.status(200).json(allProducts)
+  } catch (err) {
+    next(err)
+  }
+}
+
+//get all products
+export const getProduct = async (req, res, next) => {
+  const techno = await TechnoProduct.find()
+  const beauty = await BeautyProduct.find()
+  const haushold = await HouseholdProduct.find()
+  const productsDB = techno.concat(beauty).concat(haushold)
+
+  try {
+    const allProducts = productsDB
+    res.status(200).json(allProducts)
+  } catch (err) {
+    next(err)
   }
 }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import mongoose from 'mongoose'
+import mongoose, { mongo } from 'mongoose'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import productRoute from './routes/products.js'
@@ -11,10 +11,12 @@ const connect = () => {
   mongoose.connect(process.env.DB, console.log('DB Connected'))
 }
 
-mongoose.connection.on('disconnected', () => {
+const db = mongoose.connection
+
+db.on('disconnected', () => {
   console.log('mongoDB disconnected')
 })
-mongoose.connection.on('connected', () => {
+db.on('connected', () => {
   console.log('mongoDB connected')
 })
 
@@ -22,6 +24,17 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/products', productRoute)
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || 'Something went wrong!'
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  })
+})
 
 app.listen(8000, () => {
   connect(), console.log('Server is listening on PORT 8000')
